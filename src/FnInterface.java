a4f2c23587f4fc77806028d4e886ba2d319f1e00
hargume <hargume@dbcc6df2-7177-bd6b-6772-4166e04ea991>
2010-04-11 23:34:35 +0000

    Hice cambios, en particular para que no haya confusiones INPUT_BITS y OUTPUT_BITS

--- src/FnInterface.java
+++ src/FnInterface.java
@@ -5,16 +5,16 @@ public interface FnInterface {
 	public enum SELECT_TYPE{UNIVERSAL, ELITE, ROULETTE, ELITE_ROULETTE, ELITE_UNIVERSAL};
 	
 	/*Defino que metodo se usara para la seleccion y el reemplazo*/
-	public final SELECT_TYPE CONFIG_SELECTION = FnInterface.SELECT_TYPE.UNIVERSAL;
-	public final SELECT_TYPE CONFIG_REPLACEMENT = FnInterface.SELECT_TYPE.UNIVERSAL;
+	public final SELECT_TYPE CONFIG_SELECTION = SELECT_TYPE.UNIVERSAL;
+	public final SELECT_TYPE CONFIG_REPLACEMENT = SELECT_TYPE.UNIVERSAL;
 	
 	
 	/* Definiciones que caracterizan a la poblacion y su evolucion */	 
 	public int POPULATION_SIZE = 10;//600;
-	public double MUTATION_PROBABILITY = 0.01;	
-	public int SELECTION_SIZE = 2;//300;
+	public double MUTATION_PROBABILITY = 0.001;	
+	public int SELECTION_SIZE = 5;//300;
 	public int MAX_GENERATIONS = 10;//8;
-	public double HIGHEST_PERCENTAGE = 0.6;
+	public double HIGHEST_PERCENTAGE = 0.9;
 		
 	/* Altura maxima de los arboles que se generan para la poblacion o 
 	 * para la mutacion
@@ -27,11 +27,12 @@ public interface FnInterface {
 	 * Si son 2 entradas con 2 bits por entrada =>  A0 A1 y B0 B1 ...etc
 	 * La salida queda definida con BITS_PER_OUTPUT bits
 	 */
-	public static final int CANT_INPUTS = 2;
-	public static final int BITS_PER_INPUT = 4;
-	public static final int BITS_PER_OUTPUT = 1;
+	public static final int CANT_INPUTS = 2;	// A y B
+	public static final int INPUT_BITS = 8;		// cantidad de terminales
+	public static final int OUTPUT_BITS = 7;
 	
-	public static final String[] terminals = {"B0", "B1", "A0", "A1"};
+	public static final String[] terminals = {"B0", "B1", "B2", "B3", "A0", "A1", "A2", "A3"};
+	//public static final String[] terminals = {"B0", "A0"};
 	public static final String[] functions = {"AND", "OR", "NOT"};
 	public static final int[] arities = {2, 2, 1};
 	
